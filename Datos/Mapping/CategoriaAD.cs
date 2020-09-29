d81ced25c7df0b839a53724243ab53fd964aecc7
Loa33 <n00162403@upn.pe>
2020-09-29 17:07:58 -0500

    first commit

---
+++ Datos/Mapping/CategoriaAD.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entidades;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccesoDatos.Mapping
+{
+    public class CategoriaAD : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
